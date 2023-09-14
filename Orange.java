4cd9cd5655884320bd2d97792f78f8070a21a703
Melissandre GALLOIS <mg832620@MI105-03.iem>
2023-09-14 10:44:46 +0200

    premier commit

---
+++ Orange.java
@@ -0,0 +1,78 @@
+/**
+ *
+ * @author roudet
+ */
+public class Orange {
+    private double prix;
+    private String origine;
+	
+    public Orange() 
+    {
+        this.prix = 0.5;  //prix en euros
+        this.origine="Espagne";
+    }
+    
+    public Orange(double prix, String origine) 
+    {
+	if(prix < 0)
+	    this.prix = -prix;  //une solution possible pour interdire les prix negatifs
+	else
+	    this.prix = prix;
+
+	if(origine.equals(""))
+            this.origine = "Espagne";  //Espagne par défaut
+	else
+            this.origine = origine;   
+    }
+
+    public double getPrix(){
+	return prix;
+    }
+
+    public void setPrix(double prix){
+	this.prix=prix;
+    }
+
+    public String getOrigine(){
+	return origine;
+    }
+ 
+    public void setOrigine(String origine){
+	this.origine=origine;
+    }
+
+    @Override
+    public String toString(){
+        return "Orange de " + origine + " a " + prix + " euros";
+    }
+
+    @Override
+    public boolean equals(Object o){  //predicat pour tester si 2 oranges sont equivalentes
+        if(o != null && getClass() == o.getClass()){
+            Orange or = (Orange) o;
+            return (prix == or.prix && origine.equals(or.origine));
+        }
+        return false;
+    }
+
+    public boolean isSeedless() {  //predicat indiquant qu'une orange a des pepins
+        return false;
+    }
+
+
+    public static void main (String[] args){
+        //Ecrire ici vos tests
+	System.out.println("premier test Orange");
+    Orange or = new Orange(3, "Bresil");
+    Orange or2 = new Orange(2, "Espagne");
+    Orange or3 = new Orange(2.0, "Espagne");
+    System.out.println(or.toString());
+    
+    if(or.equals(or3)==true)
+        System.out.println("Les deux oranges sont à un prix équivalent et de meme origine.");
+    else
+        System.out.println("Ces oranges sont différentes");
+
+
+   }
+}
